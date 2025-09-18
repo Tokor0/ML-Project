2b2f7f0aceb3b2e796f10b9330d5ad61955ac83b
starkiller <santtu.turunen03@gmail.com>
2025-09-18 16:45:10 +0300

    test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+datapath=os.path.join('superconductivty+data','train.csv')
 import numpy as np
 import pandas as pd
-data=pd.read_csv('superconductivty+data\\train.csv')
+data=pd.read_csv(datapath)
 print(data.head(5))
